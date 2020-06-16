76002ffadb7747baddfa3d4670ffff9eaf16edbf
P-H-Pancholi <pancholiphalin@outlook.com>
2020-06-16 20:32:08 +0530

    Demonstrating use of Pandas module

---
+++ PythonDataP/rainfall.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#pandas Series object works as a one-dimensional array
+data = {'Month' : pd.Series(['January','February','March','April','May'
+         , 'June', 'July', 'August', 'September','October','November', 
+            'December']),
+        'Rainfall' : pd.Series([1.65, 1.25, 1.94, 2.75, 2.75, 3.645, 5.5,
+            1.0, 1.2, 1.3, 0.5, 2.3])} 
+#DataFrame needs a dictionary of Series object
+
+
+df = pd.DataFrame(data) #pandas DataFrame object looks like spreadsheet
+
+print(df)
