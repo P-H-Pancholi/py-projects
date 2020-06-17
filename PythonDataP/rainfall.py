0c6c2666253ce56102ba4ae208aff217275c658f
P-H-Pancholi <pancholiphalin@outlook.com>
2020-06-17 11:22:33 +0530

    Added methods to remove, replace and count rows with Nan

--- PythonDataP/rainfall.py
+++ PythonDataP/rainfall.py
@@ -17,3 +17,21 @@ dfj = pd.read_json(r'./rain.json')#using json file
 
 print("Our data frame:")
 print(dfj,"\n")
+
+
+#Remove rows with missing values
+dfclean = dfj.dropna(0)
+print("Our data after removing nan rows:\n",dfclean)
+
+#fill the missing values with zeros
+
+dfzeros = dfj.fillna(0)
+print("Our data with zerod values  :\n",dfzeros) 
+
+#count all rows that contains nan
+
+count = 0
+for index,row in dfj.iterrows():
+    if any(row.isnull()):
+        count += 1
+print("The number of rows that contains nun is "+ str(count))
